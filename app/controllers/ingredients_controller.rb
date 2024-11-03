e3b15b55dc9d93fe8424b2f857d4142230731720
Laura Paakkinen <la.paakkinen@gmail.com>
2024-11-03 17:00:31 +0100

    Small update to search

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -3,6 +3,7 @@ class IngredientsController < ApplicationController
     ingredients = Ingredient
       .search_by_name(params[:search])
       .order_by_recipe_count
+      .limit(50)
 
     render partial: "ingredients/search_result", layout: false, locals: { ingredients: ingredients }
   end
